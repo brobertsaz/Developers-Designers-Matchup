bdecdab2c15be06ebd1620968a5c7b07a8469444
Bob Roberts <broberts_az@yahoo.com>
2011-09-29 17:26:23 -0700

    added omniauth for twitter, nd to write tests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ provider_keys = YAML.load_file(File.join(Rails.root, 'config', 'provider_keys.ym
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
-    :facebook,
-    provider_keys[Rails.env]['facebook']['consumer_key'],
-    provider_keys[Rails.env]['facebook']['consumer_secret']
+    :twitter,
+    provider_keys[Rails.env]['twitter']['consumer_key'],
+    provider_keys[Rails.env]['twitter']['consumer_secret']
   )
 end
