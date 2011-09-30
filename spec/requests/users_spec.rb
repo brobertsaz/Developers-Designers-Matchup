f7634f6fc6c49aab9e2f4256dcab5b7faff6fa71
Bob Roberts <broberts_az@yahoo.com>
2011-09-29 18:18:13 -0700

    still cannot get test written correctly to authorize twitter

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,46 +1,15 @@
 require 'spec_helper'
 
-describe "Users Sign up" do
-  it "should not make a new user", :js => true do
-    visit new_user_registration_path
-    fill_in "Username",        :with => ""
-    fill_in "Email",        :with => ""
-    fill_in "Password",     :with => ""
-    fill_in "Password confirmation", :with => ""
-    click_on "Sign Up"
-    save_and_open_page
-    page.should have_content("Email can't be blank")
-    page.should have_content("Password can't be blank")
-  end
-
+describe "Visitor registers with omniauth" do
   it "should make a new user", :js => true do
-    visit new_user_registration_path
-    fill_in "Username",        :with => "newuser"
-    fill_in "Email",        :with => "usernew@user.com"
-    fill_in "Password",     :with => "password"
-    fill_in "Password confirmation", :with => "password"
-    click_on "Sign Up"
-    save_and_open_page
-    page.should have_content("You have signed up successfully")
+    visit root_path
+    click_on "Sign in with Twitter"
+    soap
+    page.should have_content("Authorize")
+    click_button "Sign In"
     page.should have_content("Home")
   end
 end 
 
-describe "Homepage view jobs" do 
-  before do 
-    visit root_path 
-  end 
-  it "should let visitor view developer jobs" do
-    click_link "Developer Jobs"
-    save_and_open_page
-    page.should have_content("Developer Jobs")
-  end
-  
-  it "should let visitor view designer jobs" do
-    click_link "Designer Jobs"
-    save_and_open_page
-    page.should have_content("Designer Jobs")
-  end
-end
 
 
